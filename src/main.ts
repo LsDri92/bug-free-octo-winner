bcbd5c3b119f87742e3e71ad59c0f653065049be
LsDri92 <luciano.s.dri@gmail.com>
2025-04-08 18:08:20 -0300

    new changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
 
 const savedTheme = localStorage.getItem('theme') ?? 'dark';
 document.documentElement.setAttribute('data-theme', savedTheme);
